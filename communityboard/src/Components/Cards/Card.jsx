d8323fe6cefd0f67294b47bd46980f5b02c63466
Teklemya <Teklemya@mail.uc.edu>
2024-09-23 21:00:56 -0400

    edited the links for all the cards

--- communityboard/src/Components/Cards/Card.jsx
+++ communityboard/src/Components/Cards/Card.jsx
@@ -18,52 +18,61 @@ const Card = () => {
         image={shiro}
         heading="Shiro"
         subheading="Chickpea stew"
-        link=""
+        link="https://holycowvegan.net/ethiopian-shiro-wot-ground-chickpea-stew/"
       />
       <MenuCard
         image={doro}
         heading="Doro Wat"
         subheading="Ethiopian flatbread with spicy chicken stew"
+        link="https://www.daringgourmet.com/doro-wat-spicy-ethiopian-chicken-stew/"
       />
       <MenuCard
         image={tibs}
         heading="Siga Tibs"
         subheading="Sautéed beef cubes with vegetables and spices"
+        link="https://honest-food.net/ethiopian-tibs-recipe/"
       />
       <MenuCard
         image={kitfo}
         heading="Kitfo"
         subheading="Minced raw beef seasoned with spices and clarified butter"
+        link="https://www.foodnetwork.com/recipes/kitfo-12239304"
       />
        <MenuCard
         image={firfir}
         heading="Firfir"
         subheading="Shredded injera mixed with spiced butter and berbere"
+        link="https://www.daringgourmet.com/doro-wat-spicy-ethiopian-chicken-stew/"
       />
       <MenuCard
         image={misir}
         heading="Misir Wat"
+        link="https://www.daringgourmet.com/misir-wat-ethiopian-spiced-red-lentils/"
         subheading="Red lentil stew seasoned with berbere"
       />
       <MenuCard
         image={gomen}
         heading="Gomen"
         subheading="Collard greens cooked with onions and garlic"
+        link="https://www.daringgourmet.com/gomen-ethiopian-collard-greens/"
       />
         <MenuCard
         image={atayef}
         heading="Atayef"
         subheading="Filled pancake with sweet fillings"
+        link="https://www.simplyleb.com/recipe/atayef/"
       />
       <MenuCard
         image={kik_alicha}
         heading="Kik Alicha"
         subheading="Mild split pea stew served with injera"
+        link="https://monkeyandmekitchenadventures.com/ethiopian-yellow-split-pea-stew-kik-alicha/"
       />
       <MenuCard
         image={chechebsa}
         heading="Chechebsa"
         subheading="Shredded flatbread with spiced butter"
+        link="https://tarasmulticulturaltable.com/chechebsa-ethiopian-torn-flatbread/"
       />
     </div>
   );
